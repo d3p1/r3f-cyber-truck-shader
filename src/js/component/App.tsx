3c7c7d256502e1edde42f476f5df7ecf281ebcd1
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-06 06:15:43 +0000

    feat: add cyber truck model [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,6 +2,31 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {OrbitControls} from '@react-three/drei'
+import {Canvas} from '@react-three/fiber'
+import {Suspense} from 'react'
+import {CyberTruck} from './app/CyberTruck.tsx'
+
 export const App = () => {
-  return <h1>HELLO</h1>
+  return (
+    <Canvas shadows={true} camera={{position: [6, 8, 8]}}>
+      <OrbitControls />
+
+      <ambientLight intensity={1.5} />
+      <directionalLight
+        position={[6, 8, 8]}
+        intensity={4.5}
+        castShadow={true}
+      />
+
+      <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
+        <circleGeometry args={[6]} />
+        <meshStandardMaterial color="lime" />
+      </mesh>
+
+      <Suspense>
+        <CyberTruck />
+      </Suspense>
+    </Canvas>
+  )
 }
